014bd986099dc41b5096c210b0f2da355b7d9fdc
Bas de Vreugd <basdvreugd5@msn.com>
2025-10-06 09:50:30 +0200

    feat: Add board_user pivot table

---
+++ database/migrations/2025_10_06_074424_create_board_user_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('board_user', function (Blueprint $table) {
+            $table->foreignId('board_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->primary(['board_id', 'user_id']);
+            $table->string('role')->default('member');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('board_user');
+    }
+};
