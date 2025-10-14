de58d68a558d561215666853beb3cf84238302a7
Bas de Vreugd <basdvreugd5@msn.com>
2025-10-14 09:45:03 +0200

    refractor(icon-button) made icon-button component

---
+++ resources/views/components/icon-button.blade.php
@@ -0,0 +1,24 @@
+@props(['icon', 'color' => 'default', 'disabled' => false, 'type' => 'button'])
+
+@php
+    $base = 'p-2 rounded-full transition-colors ';
+
+    $colors = [
+        'default' => 'hover:bg-slate-200 dark:hover:bg-slate-700',
+        'red' => 'text-red-500 hover:bg-red-100 dark:text-red-400 dark:hover:bg-red-900/50',
+        'slate' => 'text-slate-400 dark:text-slate-400',
+    ];
+
+    $classes = $base . ' ' . ($colors[$color] ?? $colors['default']);
+
+    if ($disabled) {
+        $classes .= ' opacity-50  text-slate-400 dark:text-slate-600';
+    }
+
+@endphp
+
+<button type="{{ $type }}" {{ $disabled ? 'disabled' : '' }} {{ $attributes->merge(['class' => $classes]) }}>
+    <span class="material-symbols-outlined !text-xl">
+        {{ $icon }}
+    </span>
+</button>
